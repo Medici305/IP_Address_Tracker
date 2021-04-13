502d09c6145f0904c432ead322316421a9fd3ba4
unknown <Medici305>
2021-04-13 02:40:42 +0100

    3. Made fetch request and displayed data.

--- app.js
+++ app.js
@@ -1,40 +1,56 @@
 // Variables
-const apiKey = "AIzaSyAuGdexQPj0tbya-NMCc2zXZIob43OPi2Y";
+const apiKey = "AIzaSyBCeLp0Wz3T0BCUOOjM64MfAfB-1jatItU";
 const ipAddress = document.querySelector(".ip_address");
-//const location = document.querySelector(".location");
+const locate = document.querySelector(".location");
 const timezone = document.querySelector(".timezone");
 const isp = document.querySelector(".isp");
+const inputSearch = document.querySelector(".input-search");
+const searchButton = document.querySelector(".btn");
+const frame = document.querySelector(".frame-map");
+const spinner = document.querySelector(".spin");
+const googleMap = document.getElementById("map");
+let serachValue;
 
 // Functions
-async function fetchIpAddress() {
+async function fetchIpAddress(searchItem) {
   try {
     const dataFetch = await fetch(
-      `https://geo.ipify.org/api/v1?apiKey=at_lMzIrsfzY0q4zhvqPnmcqAJCF2gzy&ipAddress=92.40.168.37`
+      `https://geo.ipify.org/api/v1?apiKey=at_lMzIrsfzY0q4zhvqPnmcqAJCF2gzy&ipAddress=${searchValue}`
     );
     const response = await dataFetch.json();
     console.log(response);
-    setResults(response.location);
-    showPosition(response.location);
+    // Change location displayed on map based on ip address searched for.
+    frame.src = `https://www.google.com/maps/embed/v1/place?key=AIzaSyBrt4t_3LwPHGLSBOY71_oHIZpVeUS9kHQ
+            &q=bank,${response.location.city}`;
+    // Set the results table with data returned from api.
+    setResults(response);
   } catch (error) {
     console.log(error.message);
   }
 }
 
 function setResults(obj) {
-  //ipAddress.innerText = obj
-  timezone.innerText = obj.timezone;
+  timezone.innerText = obj.location.timezone;
+  ipAddress.innerText = obj.ip;
+  locate.innerText = obj.location.city + " " + obj.location.postalCode;
+  isp.innerText = obj.isp;
 }
 
-function showPosition(position) {
-  var latlon = position.lat + "," + position.long;
-
-  var img_url = `https://maps.googleapis.com/maps/api/staticmap?center=
-    "+${latlon}+"&zoom=14&size=400x300&sensor=false&key=${apiKey}`;
-
-  document.getElementById("google_image").innerHTML =
-    "<img src='" + img_url + "'>";
+// Get the search input value
+function updateInput(e) {
+  searchValue = e.target.value;
 }
 
-fetchIpAddress();
+// Clear search input after search
+function clear() {
+  inputSearch.value = "";
+}
 
 // EventListeners
+inputSearch.addEventListener("input", updateInput);
+searchButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  clear();
+  fetchIpAddress(searchValue);
+  console.log("change");
+});
